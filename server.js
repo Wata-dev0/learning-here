f4d3e764d762383c9f7bf72c33e1bd98ab641a85
Wata-dev0 <watashi2948@gmail.com>
2025-01-11 08:55:58 -0300

    Start Day 2

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require('expess')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('public'))
+
+app.listen(3000, '0.0.0.0', () =>
+{
+    console.log('Server runing in port 3000, http://ip:3000')
+})
